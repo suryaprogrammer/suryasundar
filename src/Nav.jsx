ceba24c57eefbcb50bce7c9a50d3ab8492934ca3
surya sundar <codewithsurya7@gmail.com>
2021-11-07 14:46:46 +0530

    s

---
+++ src/Nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <>
+
+<nav id="nav">
+
+    <div id="home">
+        <img src="" alt="" />
+    </div>
+<ul>
+
+</ul>
+
+
+</nav>
+
+
+        </>
+    )
+}
+
+export default Nav
